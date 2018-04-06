1dc2afc666de4ad420dd1442a751d733048b1549
Paula García <paulaagarcia1@gmail.com>
2018-04-06 06:48:37 -0500

    Html Added :alien:

--- Semana10/index.js
+++ Semana10/index.js
@@ -1,22 +1,29 @@
-
 const http = require('http');
 //to load a file
 const fs = require('fs');
 
-
 const hostname = '127.0.0.1'
 const port = 3000;
 
+fs.readFile('index.html', (err, html) => {
+  if (err) {
+    throw err;
+  }
 
   const server = http.createServer((req, res) => {
     //200 is the  code that basically says everything is ok, when you make a request
     res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('hellou');
-  
+    res.setHeader('Content-type', 'text/html');
+    res.write(html);
+    res.end();
+  });
   server.listen(port, hostname, () => {
     console.log('Sever started on port ' + port);
+
   });
+
+
+
 });
 
 
