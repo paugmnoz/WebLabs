ce15ba551480ad4299cfe72ed8278fed1467aa46
Paula <paulaagarcia1@gmail.com>
2018-04-09 08:34:14 -0500

    :octocat:

--- Semana11/Clase/index.js
+++ Semana11/Clase/index.js
@@ -1,2 +1,17 @@
 var emoji = require('node-emoji')
 emoji.get('coffee')
+
+
+/*
+MASTER
+Everything is ok
+BRANCHES
+git branch prueba
+git checkout prueba
+
+MERGES
+git merge prueba
+
+git log
+git status
+*/
