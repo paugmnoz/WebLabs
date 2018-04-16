5cf7e802c0d1c2a989e35b7509382db377d5ce47
Paula <paulaagarcia1@gmail.com>
2018-04-16 07:45:05 -0500

    Class excercise :+1:

--- Semana11/Directory/index.js
+++ Semana11/Directory/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-app.get('/', (req, res) => res.send('Working!'));
+app.get('/', (req, res) => res.send('Hello'));
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
 
