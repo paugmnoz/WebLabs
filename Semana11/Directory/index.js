8ff32511b06cf5a93c6710517d2461c5ec56806f
Paula <paulaagarcia1@gmail.com>
2018-04-19 19:05:58 -0500

    Contact Visualization

--- Semana11/Directory/index.js
+++ Semana11/Directory/index.js
@@ -1,60 +1,33 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express'),
+    consolidate = require('consolidate'),
+    fs = require('fs'),
+    hbs = require('handlebars');
 
-app.get('/', (req, res) => res.send('Hello'));
+var app = express();
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'));
+//Configure the render engine (express + consolidate -> handlebars)
+app.engine('hbs', consolidate.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
-var file = [1];
-var text;
+app.use(express.static('public'));
+
 var name, phone, genre;
 
+app.get('/', (req, res) => {
+    //Read the directory folder
+    var directoryFolder = fs.readdirSync('../Directory/people');
+    console.log(directoryFolder);
+    directoryFolder.shift();
+    res.render('index', {
+        directoryFolder: directoryFolder
+    });
+});
 
-    for(var i = 0; i<file.length; i++){
-        file[i] = fs.readFileSync('user'+ i + '.txt');
-        text = String(file[i]).split(',');
-        name = text[0];
-        phone = text[1];
-        genre = text[2];
-        console.log('user: ' + i);
-        console.log('name: ' + name);
-        console.log('phone: ' + phone);
-        console.log('genre: ' + genre);
-    }
+app.get('/user/:id', function(req, res) {
+    res.render('contact', {
+        user: req.params.id
+    });
+});
 
-app.get('/dir', (req, res) => {
-    res.send(getHtml(name, phone, genre));
-  });
-
-//console.log('This is the file: ' + file);
-
-fs.writeFileSync('testing2.txt', 'Hola? Como estoy');
-
-var getHtml = function (name, phone, genre) {
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=<<device-width>>, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>People</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i,800" rel="stylesheet"> 
-</head>
-<body>
-    <h1>People</h1>
-    <div id="people-list">
-    <h2>${name}</h2>
-    <p>${phone}</p>
-    <p>${genre}</p>
-    </div>
-    <label for="">Name</label>
-    <input type="text" id="name" name="name">
-    <button id="show">Add</button>
-    <script src="index.js"></script> 
-</body>
-</html>
-    
-    `
-}
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
